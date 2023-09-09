2f92dca5fc201066455076c807d80a2599c7859e
109taha <109taha@gmail.com>
2023-09-09 05:00:48 -0700

    test

---
+++ src/model/blogSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema(
+  {
+    adminId: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Admin",
+    },
+    featureImg: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+    data: [
+      {
+        ctype: {
+          type: String,
+          enum: ["image", "heading", "text"],
+          require: true,
+        },
+        content: {
+          type: String,
+          require: true,
+        },
+      },
+    ],
+    Categories: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "Categories",
+    },
+    views: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+const Blog = mongoose.model("Blog", BlogSchema);
+
+module.exports = Blog;
