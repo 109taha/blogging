f716aa1b75a1079a5b39826c1293e8dd4bdf5d38
109taha <109taha@gmail.com>
2023-09-11 22:15:46 -0700

    device token pu api and remove from login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -164,6 +164,37 @@ router.post("/create/user", UserJoiSchema, async (req, res) => {
   }
 });
 
+router.put("/update/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { user_name, email, name, password, phone_number, devicetoken } =
+      req.body;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    user.user_name = user_name || user.user_name;
+    user.email = email || user.email;
+    user.name = name || user.name;
+    user.phone_number = phone_number || user.phone_number;
+    user.devicetoken = devicetoken || user.devicetoken;
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    await user.save();
+    res.status(200).send({ message: "User updated successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+});
+
 router.get("/find/user", async (req, res) => {
   try {
     const AllUser = await User.find();
@@ -191,15 +222,14 @@ router.get("/find/user/:id", async (req, res) => {
   }
 });
 
-router.post("/login/:deviceToken", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const deviceToken = req.params.deviceToken;
 
-    if (!email || !password || !deviceToken) {
+    if (!email || !password) {
       return res.status(400).json({
         success: false,
-        message: "Please provide email, password, and deviceToken.",
+        message: "Please provide email, password.",
       });
     }
 
@@ -209,9 +239,6 @@ router.post("/login/:deviceToken", async (req, res) => {
       const validAdminPassword = await bcrypt.compare(password, admin.password);
 
       if (validAdminPassword) {
-        admin.devicetoken = deviceToken;
-        await admin.save();
-
         const token = JWT.sign(
           { userId: admin._id },
           process.env.JWT_SEC_ADMIN
@@ -232,9 +259,6 @@ router.post("/login/:deviceToken", async (req, res) => {
       const validUserPassword = await bcrypt.compare(password, user.password);
 
       if (validUserPassword) {
-        user.devicetoken = deviceToken;
-        await user.save();
-
         const token = JWT.sign({ userId: user._id }, process.env.JWT_SEC);
 
         return res.status(200).json({
