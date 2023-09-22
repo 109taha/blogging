d2e6c2a8a29190dc9e7832c7dcf1b6f107a3a119
109taha <109taha@gmail.com>
2023-09-22 15:10:17 -0700

    multer pic formate added

--- src/helper/multer.js
+++ src/helper/multer.js
@@ -17,6 +17,7 @@ const imageFilter = (req, file, cb) => {
     file.mimetype === "image/jpg" ||
     file.mimetype === "image/jpeg" ||
     file.mimetype === "image/avif" ||
+    file.mimetype === "image/jfif" ||
     file.mimetype === "application/zip"
   ) {
     cb(null, true);
